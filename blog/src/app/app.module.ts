5b707402bcc89c2fdd4ad31162e79f1ccde1c650
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-16 21:33:08 +0530

    Module

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AuthUserModule} from './auth-user/auth-user.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+
 import { StudentListComponent } from './student-list/student-list.component';
 import { CountryListComponent } from './country-list/country-list.component';
+
 
 
 @NgModule({
@@ -16,7 +18,8 @@ import { CountryListComponent } from './country-list/country-list.component';
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthUserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
