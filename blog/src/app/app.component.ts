09371c47cf2f344264073ae0320fe65e28b6398e
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-20 15:29:02 +0530

    Nested Loop

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -31,11 +31,11 @@ export class AppComponent {
   color='xyz';
   users=['Rajendra','Raj','Bheem','Tom','jerry'];
   userDetails=[
-    {Name :'Rajendra', Email : 'Rajendra@email.com',phone :9876 },
-    {Name :'Raj', Email : 'Raj@email.com',phone :9876 },
-    {Name :'Bheem', Email : 'Bheem@email.com',phone :9876 },
-    {Name :'Tom', Email : 'Tom@email.com',phone :9876 },
-    {Name :'jerry', Email : 'jerry@email.com',phone :9876 },
+    {Name :'Rajendra', Email : 'Rajendra@email.com',phone :9876 ,UserAccounts:['Facebook','Gmail','Whatsapp']},
+    {Name :'Raj', Email : 'Raj@email.com',phone :9876 ,UserAccounts:['Instagram','Gmail','Google']},
+    {Name :'Bheem', Email : 'Bheem@email.com',phone :9876,UserAccounts:['Apple','Samsung ','Whatsapp'] },
+    {Name :'Tom', Email : 'Tom@email.com',phone :9876 ,UserAccounts:['Yahoo','Gmail','Telegram']},
+    {Name :'jerry', Email : 'jerry@email.com',phone :9876 ,UserAccounts:['Twitter','bard','ChatGpt']},
 
   ];
 }
