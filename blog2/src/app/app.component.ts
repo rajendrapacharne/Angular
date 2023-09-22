b66a3133e91004966b41688ae03fa094e82eee2c
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-22 16:29:30 +0530

    Todo list in angular

--- blog2/src/app/app.component.ts
+++ blog2/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog2';
+  list:any[]=[];
+  addtask(item:string)
+  {
+    this.list.push({id:this.list.length,name:item});
+    console.warn(this.list);
+  }
+  removeTask(id:number)
+  {
+    console.warn(id);
+    this.list=this.list.filter(item=>item.id!==id);
+  }
 }
