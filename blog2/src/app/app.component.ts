4006ee91715e49c8a8630380ffb88893060d9a5f
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-25 18:16:20 +0530

    Reactive form Validations

--- blog2/src/app/app.component.ts
+++ blog2/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl} from '@angular/forms';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators ,ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -62,12 +62,20 @@ export class AppComponent {
   // }
 
   loginForm=new FormGroup({
-    user:new FormControl(''),
-    password:new FormControl('')
+    user:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
   
   })
   LoginUser()
   {
     console.warn(this.loginForm.value);
+  }
+  get user()
+  {
+    return this.loginForm.get('user');
+  }
+  get password()
+  {
+    return this.loginForm.get('password');
   }
 }
