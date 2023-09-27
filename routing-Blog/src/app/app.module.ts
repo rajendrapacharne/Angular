fbb1ca3d0b844001b94ca4e0f6cdca45d4328eb4
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-27 11:31:17 +0530

    Child Routing

--- routing-Blog/src/app/app.module.ts
+++ routing-Blog/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NoPageComponent } from './no-page/no-page.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NoPageComponent } from './no-page/no-page.component';
     UserComponent,
     HomeComponent,
     AboutComponent,
-    NoPageComponent
+    NoPageComponent,
+    AboutCompanyComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
