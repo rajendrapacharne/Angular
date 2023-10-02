7b3ce5bdde1ad3bccc48a75e9f4c79d09eb3a378
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-10-02 17:19:45 +0530

    First Unit test case

---
+++ blog-last/src/app/dummy.spec.ts
@@ -0,0 +1,9 @@
+describe('dummy file', () => {
+    it("should check if the amount is equal", () => {
+        expect(100).toBe(100);
+    });
+
+    it("should check if the amount is equal", () => {
+        expect(100).toBe(100);
+    });
+});
