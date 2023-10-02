37efefeb9296b1364107f8c020ba65e1ea8802a3
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-10-02 15:35:14 +0530

    Lazy Loading component

--- blog-avd/src/app/app.component.ts
+++ blog-avd/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewContainerRef,ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog-avd';
+
+  constructor(private viewContainer:ViewContainerRef,private cfr:ComponentFactoryResolver)
+  {
+
+  }
+  async loadAdmin()
+  {
+    this.viewContainer.clear();
+    const {LoginComponent} = await import ("./admin/login/login.component");
+    this.viewContainer.createComponent(
+      this.cfr.resolveComponentFactory(LoginComponent)
+    )
+  }
+  async loadUser()
+  {
+    this.viewContainer.clear();
+    const {LoginComponent} =await import ("./user/login/login.component");
+    this.viewContainer.createComponent(
+      this.cfr.resolveComponentFactory(LoginComponent)
+    )
+  }
 }
